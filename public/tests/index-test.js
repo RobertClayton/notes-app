037e8f86567c0d30a0b76efcc3de59de356fe7a3
Robert Clayton <robbie.c.clayton@gmail.com>
2017-12-07 11:19:24 +0000

    added personal URLs and href tags

---
+++ public/tests/index-test.js
@@ -0,0 +1,6 @@
+function testNoteListControllerIncludesString() {
+
+  assert.isTrue();
+}
+
+testNoteListControllerChangesHTML();
--- public/views/note-list-views.js
+++ public/views/note-list-views.js
@@ -5,7 +5,7 @@
 
   NoteListView.prototype.returnHTML = function() {
     var noteListMap = this.noteList.list.map(function(listItem) {
-      return ("<li><div>" + listItem.text.slice(0,20) + "</div></li>");
+      return (`<li><div><a href="#id${listItem.id}">${listItem.text.slice(0,20)}</a></div></li>`);
     });
     return ("<ul>" + noteListMap.join("") + "</ul>");
   };
